e5995f6a8f2f6a518486e0a9c7e978aee6bc42de
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-28 12:53:41 +0300

    add login and logout

--- sales_funnel/user_auth/forms.py
+++ sales_funnel/user_auth/forms.py
@@ -1,11 +1,6 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=40)
+    password = forms.CharField(max_length=40, widget=forms.PasswordInput)
--- sales_funnel/user_auth/views.py
+++ sales_funnel/user_auth/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm
 
-from .forms import UserRegisterForm
 
-
-def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+def sign_in(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
             return redirect('index')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'user_auth/register.html', {'form': form})
+
+
+        form = LoginForm()
+        return render(request,'user_auth/login.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                login(request, user)
+                messages.success(request,f'Hi {username.title()}, welcome back!')
+                return redirect('index')
+
+        # either form not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'user_auth/login.html',{'form': form})
+
+
+def sign_out(request):
+    logout(request)
+    messages.success(request, f'You have been logged out.')
+    return redirect('login')  
