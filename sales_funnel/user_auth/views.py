ab6e801c2badae4091e79365d21b2a21197da39b
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-28 19:18:00 +0300

    add registration

--- sales_funnel/user_auth/views.py
+++ sales_funnel/user_auth/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from .forms import LoginForm
+
+from .forms import LoginForm, RegisterForm
 
 
 def sign_in(request):
@@ -34,3 +35,21 @@ def sign_out(request):
     logout(request)
     messages.success(request, f'You have been logged out.')
     return redirect('login')  
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'user_auth/register.html', {'form': form})    
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'user_auth/register.html', {'form': form})
